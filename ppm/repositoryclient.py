0e45b12b032c6db099957c2c7b7450092158ebf3
Amine Hajyoussef <hajyoussef.amine@gmail.com>
2014-02-11 10:19:48 +0100

    fix a bug in package existence test

--- ppm/repositoryclient.py
+++ ppm/repositoryclient.py
@@ -12,7 +12,9 @@ class RepositoryClient:
         try:
             r = requests.post(self.server_adress,origin,timeout=REQUEST_TIMEOUT)
             if r.status_code == 200:
-                return r.text            
+                return r.text
+            elif r.status_code == 404:
+                log("file not found in the repository")
         except requests.exceptions.RequestException as e:
             log("Connection error to repository server, "+self.server_adress+": "+str(e))
             return None
